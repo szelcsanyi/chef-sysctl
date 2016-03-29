67baad82925601a44a7119ee3002da180d5580be
szelcsanyi <szelcsanyi.gabor@gmail.com>
2016-03-29 14:59:10 +0000

    lint fix

--- recipes/tcp_fastopen.rb
+++ recipes/tcp_fastopen.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: L7-sysctl
 # Recipe:: tcp_fastopen
 #
-# Copyright 2015, Gabor Szelcsanyi <szelcsanyi.gabor@gmail.com>
+# Copyright 2016, Gabor Szelcsanyi <szelcsanyi.gabor@gmail.com>
 
 L7_sysctl 'net.ipv4.tcp_fastopen' do
   comment 'Enable client/server side tcp fastopen'
@@ -10,13 +10,14 @@ L7_sysctl 'net.ipv4.tcp_fastopen' do
   immediately true
 end
 
-require "digest/md5"
+require 'digest/md5'
 checksum   = Digest::MD5.hexdigest(node['fqdn'] + '-hostname')
-fastopen_key = checksum[0, 8] + '-' + checksum[8, 8] + '-' + checksum[16, 8] + '-' + checksum[24, 8]
+fo_key = checksum[0, 8] + '-' + checksum[8, 8] + '-' + \
+         checksum[16, 8] + '-' + checksum[24, 8]
 
 L7_sysctl 'net.ipv4.tcp_fastopen_key' do
   comment 'Set fastopen key to a hostname based value'
-  value fastopen_key
+  value fo_key
   immediately true
 end
 
