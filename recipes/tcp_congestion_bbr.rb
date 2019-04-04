6a9a7651e8d67373d2e4de948c306281c11ed711
szelcsanyi <szelcsanyi.gabor@gmail.com>
2019-04-04 14:23:16 +0000

    add bbr support

---
+++ recipes/tcp_congestion_bbr.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: L7-sysctl
+# Recipe:: tcp_congestion_yeah
+#
+# Copyright 2019, Gabor Szelcsanyi <szelcsanyi.gabor@gmail.com>
+
+# Not in openvz guest
+return if File.exist?('/proc/vz')
+
+# Not under kernel version 4.9
+kmajor, kminor = node[:kernel][:release].split('.')
+
+if (kmajor.to_i==4 and kminor.to_i>=9) or kmajor.to_i>4
+
+  L7_sysctl 'net.core.default_qdisc' do
+    value 'fq'
+  end
+
+  L7_sysctl 'net.ipv4.tcp_congestion_control' do
+    value 'bbr'
+  end
+
+end
--- recipes/tcp_congestion_yeah.rb
+++ recipes/tcp_congestion_yeah.rb
@@ -7,22 +7,6 @@
 # Not in openvz guest
 return if File.exist?('/proc/vz')
 
-bash 'insert tcp-yeah kernel module' do
-  user 'root'
-  code <<-EOH
-  modprobe tcp-yeah
-  EOH
-  not_if 'lsmod | grep -cq tcp_yeah'
-end
-
-bash 'add tcp-yeah module to modules' do
-  user 'root'
-  code <<-EOH
-  echo 'tcp-yeah' >> /etc/modules
-  EOH
-  not_if 'grep "^tcp-yeah$" /etc/modules'
-end
-
 L7_sysctl 'net.ipv4.tcp_congestion_control' do
   value 'yeah'
 end
